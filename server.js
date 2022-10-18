a96239844d2ff06f51fa6c37ef3c943945d7f795
Victor Apaez <vic.apaez@gmail.com>
2022-10-18 16:39:35 -0400

    created express app on localhost 3000

---
+++ server.js
@@ -0,0 +1,20 @@
+import db from "./db/connection.js";
+import routes from "./routes/index.js"; // not created yet
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/", routes);
+
+db.on("connected", () => {
+  console.clear();
+  console.log("Connected to MongoDB!");
+  app.listen(PORT, () => {
+    console.log(
+      `Express server running in development on http://localhost:${PORT}`
+    );
+  });
+});
