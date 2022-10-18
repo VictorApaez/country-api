4824ebea766d087573b8a09d044529664e531976
Victor Apaez <vic.apaez@gmail.com>
2022-10-18 16:42:58 -0400

    created countries/ route with getCountries method

--- seed/data.js
+++ seed/data.js
@@ -1,11 +1,40 @@
 import fetch from "node-fetch";
+import db from "../db/connection.js";
+import Country from "../models/Country.js";
 let urlTopMovies = `https://restcountries.com/v3.1/all`;
 
 function fetchMovies() {
   fetch(urlTopMovies)
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      let api = trimData(res);
+      seedData(api);
     });
 }
 fetchMovies();
+
+function trimData(api) {
+  return api.map((country) => {
+    let { name, capital, languages, maps, population, continents, flags } =
+      country;
+    return {
+      name,
+      capital,
+      languages,
+      maps,
+      population,
+      continents,
+      flags,
+    };
+  });
+}
+
+async function seedData(api) {
+  try {
+    await Country.deleteMany({});
+    await Country.create(api);
+    await db.close();
+  } catch (error) {
+    console.log("Data.js Error: " + error);
+  }
+}
