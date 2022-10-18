2b15dfca935d8741bb38857f7a4810e05beafe7e
Victor Apaez <vic.apaez@gmail.com>
2022-10-18 15:41:41 -0400

    created connection to country-api database

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+mongoose.set("returnOriginal", false);
+
+mongoose.connect("mongodb://127.0.0.1:27017/country-api").catch((err) => {
+  console.log(`Error connection go MongoDB: ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from MongoDB!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`MongoDB connection error: ${err}`);
+});
+
+export default mongoose.connection;
