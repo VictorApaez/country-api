66f9e5ec9a8cd4bffdb1b5f19fc47df20c779af0
Victor Apaez <vic.apaez@gmail.com>
2022-10-18 16:38:18 -0400

    added console log for successful db connection

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/country-api").catch((err) => {
   console.log(`Error connection go MongoDB: ${err.message}`);
 });
 
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB!");
+});
+
 mongoose.connection.on("disconnected", () => {
   console.log("Disconnected from MongoDB!");
 });
