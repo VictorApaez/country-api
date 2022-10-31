cfd86efe83ad69a13d8a28ea676724f24b8f09ee
Victor Apaez <vic.apaez@gmail.com>
2022-10-31 13:44:50 -0400

    handling cors error

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 app.use("/", routes);
 app.use(express.static("home-page"));
